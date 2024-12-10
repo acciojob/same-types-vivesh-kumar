fbeffcf5b21172dda23dd4703dc218b41cea1f3e
Vivesh kumar <vivesh103@gmail.com>
2024-12-10 20:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function isSameType(value1, value2) {
   //your js code here
-		const parsedvalue1 = is.NaN(value1)?value1:Number(value1);
-	    const parsedvalue2 = is.NaN(value2)? value2:Number(value2);
+		const parsedvalue1 = isNaN(value1)?value1:Number(value1);
+	    const parsedvalue2 = isNaN(value2)? value2:Number(value2);
 	
 	if (Number.isNaN(parsedvalue1)&& Number.isNaN(parsedvalue2)){
 		return true;
